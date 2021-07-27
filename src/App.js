46d0d7c141755bc547c74d441c445b7e0de31e4f
khoramshahy <shkhoramshahy@gmail.com>
2021-07-27 10:03:23 -0700

    add autocomplete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import React, { useState, useRef, useEffect } from "react";
 //mapbox
 import mapboxgl from "mapbox-gl";
+//material-ui
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
 //css
 import './App.css'
 
@@ -13,6 +17,12 @@ var map;
 const App = () => {
     //mapbox
     const mapContainerRef = useRef(null);
+    //autocomplete
+    const [open, setOpen] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [options, setOptions] = useState([{name: 'sss'},{name: 'fff'},{name: 'ggg'},{name: 'jjj'}]);
+    const [selectedOptions, setSelectedOptions] = useState([{name: 'ggg'},{name: 'jjj'},]);
+    const [loading, setLoading] = useState(false);
 
     // initialize map when component mounts
   useEffect(() => {
@@ -41,7 +51,52 @@ const App = () => {
   }, []); 
 
     return (
-        <div ref={mapContainerRef} className="map-container" />
+        <>
+            <div className="sidebar">
+                <Autocomplete
+                    id="countries-list"
+                    multiple
+                    className="auto-complete"
+                    loadingText="Searching..."
+                    open={open}
+                    onOpen={() => {
+                        setOpen(true);
+                    }}
+                    onClose={() => {
+                        setOpen(false);
+                    }}
+                    onChange={(event, newValue) => {
+                        setSelectedOptions(newValue);
+                    }}
+                    onInputChange={(event, newInputValue) => {
+                        setInputValue(newInputValue);
+                    }}
+                    getOptionSelected={(option, value) => option.name === value.name}
+                    getOptionLabel={(option) => option.name}
+                    autoHighlight
+                    filterSelectedOptions
+                    options={options}
+                    loading={loading}
+                    renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="choose country"
+                        variant="outlined"
+                        InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                            {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                        }}
+                    />
+                    )}
+                />
+            </div>
+            <div ref={mapContainerRef} className="map-container" />
+        </>
     )
 }
 
