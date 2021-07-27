6b45e77a1cc6640e5f6d20ef5a431585356dca5d
khoramshahy <shkhoramshahy@gmail.com>
2021-07-27 08:33:50 -0700

    initial App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//react
+import ReactDOM from "react-dom";
+import React, { useState, useRef, useEffect } from "react";
+
+
+const App = () => {
+
+    return (
+        <div>Hi</div>
+    )
+}
+
+export default App;
